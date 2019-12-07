3c0c3d151561741f860ee4c461da7ad451f8a2b9
mi-frutuoso <mariacfines@gmail.com>
2019-12-07 01:19:09 +0000

    voter fix

--- voter.sh
+++ voter.sh
@@ -5,11 +5,13 @@
 # get nCandidates
 echo "Insert number of candidates: "
 read nCandidates
-echo $nCandidates
 
 # initialize output files
+voter=$(basename "`pwd`")
+cryptFile="cryptVotes_${voter}.txt"
 touch votelist.txt # useful for debug
-touch cryptVotes.txt
+touch $cryptFile
+
 # get votes for each candidate
 for ((i=1;i<=${nCandidates};i++))
 do
@@ -19,7 +21,7 @@ do
     cat vote.txt >> votelist.txt # useful for debug
     # step 2) Encrypts the vote using the election public key and Microsoft seal library;
     ./weights_encryptor vote
-    cat encrypted.txt >> cryptVotes.txt
+    cat encrypted.txt >> $cryptFile
     rm -r vote.txt
     rm -r encrypted.txt
 done
@@ -28,26 +30,23 @@ done
 datetime=$(date +'%d%m%Y_%H%M%S')
 
 echo "${datetime}"
-echo "$(cat cryptVotes.txt)$datetime" > cryptVotes.txt #optimize this
+echo "$(cat $cryptFile)$datetime" > $cryptFile # display datetime
 
 # 3) Signs the vote using the voter’s private key, using the libcryp library;
 
 # first prepare input file
-voter=$(basename "`pwd`")
-echo "${voter}"
 privateKeyFile="${voter}.pem"
-echo "${privateKeyFile}"
 signatureFile="signature_${voter}.txt"
 
 # sign
-openssl dgst -sha256 -sign $privateKeyFile -out sign.sha256 cryptVotes.txt # binary file
-openssl base64 -in sign.sha256 -out $signatureFile                         # base64 format
+openssl dgst -sha256 -sign $privateKeyFile -out /tmp/sign.sha256 $cryptFile # binary file
+openssl base64 -in /tmp/sign.sha256 -out $signatureFile                         # base64 format
 
 # 4) Cast the vote and sends it to the ballot box.
-#cp cryptVotes.txt ../../BallotBox
+#cp $cryptFile ../../BallotBox
 
 # generate public key to send to ballot box
 publicKeyFile="${voter}_public.key"
 openssl rsa -in $privateKeyFile -pubout -out $publicKeyFile
 
-#cp publicKeyFile ../../BallotBox
+#cp $publicKeyFile ../../BallotBox
