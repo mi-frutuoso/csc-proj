d9e3a20bd1f8241c8f22b2ce35f88e46e61b4eed
mi-frutuoso <mariacfines@gmail.com>
2019-12-07 19:04:26 +0000

    voter: one file per vote per candidate

--- voter.sh
+++ voter.sh
@@ -6,47 +6,49 @@
 echo "Insert number of candidates: "
 read nCandidates
 
+# add read robustness
+
 # initialize output files
 voter=$(basename "`pwd`")
-cryptFile="cryptVotes_${voter}.txt"
 touch votelist.txt # useful for debug
-touch $cryptFile
-
 # get votes for each candidate
 for ((i=1;i<=${nCandidates};i++))
 do
     echo "Insert your vote for candidate ${i}: "
     read vote
-    printf $vote >> vote.txt
+    printf "${vote}\n" >> vote.txt
     cat vote.txt >> votelist.txt # useful for debug
-    # step 2) Encrypts the vote using the election public key and Microsoft seal library;
-    ./weights_encryptor vote
-    cat encrypted.txt >> $cryptFile
     rm -r vote.txt
-    rm -r encrypted.txt
+    
+    # step 2) Encrypts the vote using the election public key and Microsoft seal library;
+    ./weights_encryptor $vote # --> generate encrypted.txt
+
+    # add day + time
+    datetime=$(date +'%d%m%Y-%H%M%S')
+    candidateVoteFile="crypt_${voter}_cand${i}_${datetime}.txt"
+    # rename file
+    mv encrypted.txt ${candidateVoteFile}
+
+    # 3) Signs the vote using the voter’s private key, using the libcryp library;
+    # first prepare input file
+    privateKeyFile="${voter}.pem"
+    signatureFile="signature_${voter}_${i}_${datetime}.txt"
+
+    # sign
+    openssl dgst -sha256 -sign $privateKeyFile -out sign.sha256 ${candidateVoteFile}    # binary file
+    openssl base64 -in sign.sha256 -out $signatureFile                                  # base64 format
+    rm sign.sha256
+    
+    # 4) Cast the vote and sends it to the ballot box.
+    mv ${candidateVoteFile} ../../BallotBox
+    # send signature
+    mv $signatureFile ../../BallotBox
+
 done
 
-# # add day + time
-datetime=$(date +'%d%m%Y_%H%M%S')
-
-echo "${datetime}"
-echo "$(cat $cryptFile)$datetime" > $cryptFile # display datetime
-
-# 3) Signs the vote using the voter’s private key, using the libcryp library;
-
-# first prepare input file
-privateKeyFile="${voter}.pem"
-signatureFile="signature_${voter}.txt"
-
-# sign
-openssl dgst -sha256 -sign $privateKeyFile -out /tmp/sign.sha256 $cryptFile # binary file
-openssl base64 -in /tmp/sign.sha256 -out $signatureFile                         # base64 format
-
-# 4) Cast the vote and sends it to the ballot box.
-#cp $cryptFile ../../BallotBox
 
 # generate public key to send to ballot box
 publicKeyFile="${voter}_public.key"
 openssl rsa -in $privateKeyFile -pubout -out $publicKeyFile
 
-#cp $publicKeyFile ../../BallotBox
+mv $publicKeyFile ../../BallotBox
